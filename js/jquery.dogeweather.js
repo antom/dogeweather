23bcc371b0a4da817791798ac75bda949f95bc39
Andy Thomas <git@antom.co.uk>
2014-02-14 19:15:06 +0000

    Such html title, very random number fix.

--- js/jquery.dogeweather.js
+++ js/jquery.dogeweather.js
@@ -6,6 +6,7 @@ $('.geo').bind('click', function(e) {
         clearInterval(window.doge_interval);
         delete window.doge_interval;
         $('.such span').remove();
+        $('title').text('such weather');
     } else if (btn.text() == 'very location') {
         if (navigator.geolocation) {
             btn.text('so finding…');
@@ -43,9 +44,7 @@ $('.geo').bind('click', function(e) {
                 if (!arr) arr = doge.tings;
                 return arr[Math.floor(Math.random() * arr.length)];
             },
-            suchtemp = parseInt(doge.temperature.celsius,10),
-            interval,
-            colour_class;
+            suchtemp = parseInt(doge.temperature.celsius,10);
         
         $('body').attr(
             'class',
@@ -61,27 +60,33 @@ $('.geo').bind('click', function(e) {
         }
      
         window.doge_interval = setInterval(function() {
-            var l  = 5 + Math.round(Math.random() * 90) + '%',
+            var colour_class = Math.floor(Math.random() * 20) + 1,
+                l  = 5 + Math.round(Math.random() * 90) + '%',
                 t  = 5 + Math.round(Math.random() * 90) + '%',
                 fs = Math.max(20, Math.round(Math.random() * 50 + 24)) + 'px',
-                s = r([
+                title = $('title'),
+                rs = Math.floor(Math.random() * 2) + 1,
+                s = [
                     'so wow', 'such ' + r(),
                     'very ' + r(), 'much ' + r(),
                     'so ' + r(),
                     'wow'
-                ]);
+                ];
             
-            colour_class = Math.floor(Math.random() * 20);
             colour_class = 'c' + (colour_class < 10 ? '0' : '') + colour_class;
             
             $('.such.overlay').append(
                 '<span style="position:absolute;left:' + l + ';top:' + t + ';font-size:' + fs + ';" class="' + colour_class + '">'
-                    + s +
+                    + r(s) +
                 '</span>'
             );
             
             if ($('.such span').length > 8) {
                 $('.such span:nth-child(1)').remove();
+            }
+
+            if (rs == 2) {
+                title.text(r(s));
             }
         },2500);
     };
